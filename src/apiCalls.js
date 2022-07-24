cb4f03dc8f461a6368996e809634158c6caa2f22
Stephanie Guzman <sguzman247@gmail.com>
2022-07-24 16:50:54 -0500

    Refactor api helper function

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,38 +1,7 @@
-// Your fetch requests will live here!
-const finalDestination = `https://whats-cookin-api-data.herokuapp.com`;
-
-
-const getData = Promise.all([
-  fetch(`${finalDestination}/api/v1/users`)
-    .then(response => response.json()),
-  fetch(`${finalDestination}/api/v1/recipes`)
-    .then(response => response.json()),
-  fetch(`${finalDestination}/api/v1/ingredients`)
+var getData = (url) => {
+  return fetch(url)
     .then(response => response.json())
-])
-
-
-function loadUsers() {
-  return fetch(`${finalDestination}/api/v1/users`)
-    .then(response => response.json())
-    .then(data => data)
-    .catch(error => console.error(error))
-}
-
-function loadIngredients() {
-  return fetch(`${finalDestination}/api/v1/ingredients`)
-    .then(response => response.json())
-    .then(data => data)
-    .catch(error => console.error(error))
-}
-
-function loadRecipes() {
-  return fetch(`${finalDestination}/api/v1/recipes`)
-    .then(response => response.json())
-    .then(data => data)
-    .catch(error => console.log(error))
+    .catch(error => console.log('Looks like there was a problem!', error))
 }
 
 export default getData;
-export { loadUsers, loadIngredients, loadRecipes }
-console.log('I will be a fetch request!')
