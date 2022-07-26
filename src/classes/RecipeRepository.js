97f44a648a7342b81447683f7e956965454c6c14
Patrick Becker <patrickgbecker@gmail.com>
2022-07-25 20:46:42 -0500

    Refactor filter by tag functions

--- src/classes/RecipeRepository.js
+++ src/classes/RecipeRepository.js
@@ -5,7 +5,14 @@ class RecipeRepository {
   constructor(recipeData) {
     this.recipes = recipeData;
   }
-  
+
+  getRecipesInfo(ingredientsData) {
+    this.recipes = this.recipes.map(recipe => new Recipe(recipe));
+    this.recipes.forEach((recipe) => {
+      recipe.getIngredientsData(ingredientsData);
+    });
+  }
+
   filterByTag(tagType) {
     const recipes = this.recipes;
     const filteredRecipesByTags = recipes
@@ -13,7 +20,7 @@ class RecipeRepository {
       .map(recipe => recipe);
     return filteredRecipesByTags;
   }
-  
+
   filterByRecipeName(recipeName) {
     const recipes = this.recipes;
     const filteredRecipesByName = recipes
@@ -23,25 +30,38 @@ class RecipeRepository {
   }
 
   getRecipeIngredientsData(searchInput) {
-    const recipes = this.recipes;
-    const filteredRecipes = recipes
-      .reduce((searchedRecipes, recipe) => {
-        if (recipe.name === searchInput) {
-          searchedRecipes.push(recipe);
-        } else {
-          const matchedIngredient = recipe.ingredients
-            .find(ingredient => ingredient.name === searchInput)
+      const recipes = this.recipes;
+      const filteredRecipes = recipes
+        .reduce((searchedRecipes, recipe) => {
+          if (recipe.name === searchInput) {
+            searchedRecipes.push(recipe);
+          } else {
+            const matchedIngredient = recipe.ingredients
+            .find((ingredient) => {
+              return ingredient.name.includes(searchInput);
+            });
             if(matchedIngredient) {
               searchedRecipes.push(recipe);
             }
-            console.log(searchedRecipes);
-          }  
-          return searchedRecipes
-        }, []);
-      return filteredRecipes;
-    }
-  };
-  
+          }
+            return searchedRecipes
+          }, []);
+          // console.log('filteredRecipes', filteredRecipes);
+        return filteredRecipes;
+      }
+  // getRecipeIngredientsData(ingredientName, recipesToSearch) {
+  //   const recipes = recipesToSearch || this.recipes;
+  //   const filteredRecipe = recipes.filter((recipe) => {
+  //     const hasMatchingIngredient = recipe.ingredients.find((ingredient) => {
+  //       return ingredient.name.includes(ingredientName);
+  //     })
+  //     if (hasMatchingIngredient) {
+  //       return true;
+  //     }
+  //     return false;
+  //   })
+  //   return filteredRecipe;
+  // }
   // getRecipeIngredientsData(searchInput) {
   //   const recipes = this.recipes || searchInput ;
   //   const filteredRecipe = recipes.filter((recipe) => {
@@ -55,15 +75,7 @@ class RecipeRepository {
   //   })
   //   return filteredRecipe;
   // }
-  
-//   getRecipeInfo(ingredientsData) {
-//     const recipes = this.recipes;
-//     const recipeInfo = new Recipe(recipes)
-//       .forEach(recipe => recipe.getIngredientsData(ingredientsData))
-//       .map(recipe => recipe);
-//     return recipeInfo;
-//   }
 
-// };
+};
 
 export default RecipeRepository;
--- test/RecipeRepository-test.js
+++ test/RecipeRepository-test.js
@@ -1,5 +1,7 @@
 import { expect } from 'chai';
 import { recipeData } from '../src/data/sampleRecipeData.js';
+import { ingredientsData } from '../src/data/sampleIngredientData';
+import Ingredient from '../src/classes/Ingredient';
 import RecipeRepository from '../src/classes/RecipeRepository';
 
 describe('Recipe Repository', () => {
@@ -7,6 +9,7 @@ describe('Recipe Repository', () => {
 
   beforeEach( () => {
     repository = new RecipeRepository(recipeData);
+    repository.getRecipesInfo(ingredientsData);
   })
 
   it('Should be a function', () => {
@@ -18,7 +21,7 @@ describe('Recipe Repository', () => {
   });
 
   it('should have recipes', () => {
-    expect(repository.recipes).to.deep.equal(recipeData)
+    expect(repository.recipes[0].id).to.equal(recipeData[0].id)
   });
 
   it('should have tags for each recipe', () => {
@@ -28,41 +31,38 @@ describe('Recipe Repository', () => {
   });
 
   it('should have ingredient information for a recipe', () => {
-    const recipeIngredients1 = [
-      { id: 20081, quantity: { amount: 1.5, unit: 'c' } },
-      { id: 18372, quantity: { amount: 0.5, unit: 'tsp' } },
-      { id: 1123, quantity: { amount: 1, unit: 'large' } },
-      { id: 19335, quantity: { amount: 0.5, unit: 'c' } },
-      { id: 19206, quantity: { amount: 3, unit: 'Tbsp' } },
-      { id: 19334, quantity: { amount: 0.5, unit: 'c' } },
-      { id: 2047, quantity: { amount: 0.5, unit: 'tsp' } },
-      { id: 1012047, quantity: { amount: 24, unit: 'servings' } },
-      { id: 10019903, quantity: { amount: 2, unit: 'c' } },
-      { id: 1145, quantity: { amount: 0.5, unit: 'c' } },
-      { id: 2050, quantity: { amount: 0.5, unit: 'tsp' } }
-    ]
-    
-    expect(repository.recipes[0].ingredients).to.deep.equal(recipeIngredients1);
-  });
+      const newRepo = new RecipeRepository(recipeData);
+      let ingredient1 = newRepo.recipes[0].ingredients[0];
 
-  it('should have ingredient information for a different recipe', () => {
-    const recipeIngredients2 = [
-      { id: 20081, quantity: { amount: 1, unit: 'cup' } },
-      { id: 18371, quantity: { amount: 2, unit: 'teaspoons' } },
-      { id: 9040, quantity: { amount: 12, unit: 'servings' } },
-      { id: 20011, quantity: { amount: 1, unit: 'cup' } },
-      { id: 1001, quantity: { amount: 2, unit: 'tablespoons' } },
-      { id: 1001, quantity: { amount: 6, unit: 'tablespoons' } },
-      { id: 1230, quantity: { amount: 2, unit: 'cups' } },
-      { id: 1123, quantity: { amount: 2, unit: '' } },
-      { id: 19296, quantity: { amount: 12, unit: 'servings' } },
-      { id: 16098, quantity: { amount: 12, unit: 'servings' } },
-      { id: 2047, quantity: { amount: 1, unit: 'teaspoon' } },
-      { id: 19335, quantity: { amount: 2, unit: 'teaspoons' } }
-    ]
+      expect(ingredient1.name).to.equal(undefined);
+      expect(ingredient1).not.instanceOf(Ingredient);
 
-    expect(repository.recipes[1].ingredients).to.deep.equal(recipeIngredients2)
-  })
+      newRepo.getRecipesInfo(ingredientsData);
+      ingredient1 = newRepo.recipes[0].ingredients[0];
+
+      expect(ingredient1.name).to.equal('wheat flour');
+      expect(ingredient1).instanceOf(Ingredient);
+    });
+
+
+  // it('should have ingredient information for a different recipe', () => {
+  //   const recipeIngredients2 = [
+  //     { id: 20081, quantity: { amount: 1, unit: 'cup' } },
+  //     { id: 18371, quantity: { amount: 2, unit: 'teaspoons' } },
+  //     { id: 9040, quantity: { amount: 12, unit: 'servings' } },
+  //     { id: 20011, quantity: { amount: 1, unit: 'cup' } },
+  //     { id: 1001, quantity: { amount: 2, unit: 'tablespoons' } },
+  //     { id: 1001, quantity: { amount: 6, unit: 'tablespoons' } },
+  //     { id: 1230, quantity: { amount: 2, unit: 'cups' } },
+  //     { id: 1123, quantity: { amount: 2, unit: '' } },
+  //     { id: 19296, quantity: { amount: 12, unit: 'servings' } },
+  //     { id: 16098, quantity: { amount: 12, unit: 'servings' } },
+  //     { id: 2047, quantity: { amount: 1, unit: 'teaspoon' } },
+  //     { id: 19335, quantity: { amount: 2, unit: 'teaspoons' } }
+  //   ]
+  //
+  //   expect(repository.recipes[1].ingredients).to.deep.equal(recipeIngredients2)
+  // })
 
   it('should filter recipes by tags', () => {
     const expected1 =  repository.filterByTag('appetizer')
@@ -93,32 +93,32 @@ describe('Recipe Repository', () => {
 
   it('should not return a recipe if name is not found', () => {
     const expected = repository.filterByRecipeName('Creme L\'ainglaise')
-    
-    expect(expected).to.deep.equal([]);
-    expect(expected.name).to.equal(undefined);
-  })
-  
-  it('should get recipes that includes search term', () => {
-    const expected1 = repository.getRecipeIngredientsData('Loaded Chocolate Chip Pudding Cookie Cups');
-    const expected2 = repository.getRecipeIngredientsData('instant vanilla pudding');
-    const expected3 = repository.getRecipeIngredientsData('proscuitto');
-    
-    expect(expected1).to.deep.equal([repository.recipes[0]])
-    expect(expected2).to.deep.equal([repository.recipes[1]])
-    expect(expected3).to.deep.equal([repository.recipes[2]])
-  });
-  
-  it('should return an empty array if no recipes match the search term', () => {
-    const expected = repository.getRecipeIngredientsData('broccoli');
-    
+
     expect(expected).to.deep.equal([]);
     expect(expected.name).to.equal(undefined);
   })
 
-  it.skip('should get all data for a given recipe', () => {
-    const expected1 = repository.getRecipeInfo();
-    const expected2 = repository.getRecipeInfo();
-    const expected3 = repository.getRecipeInfo();
+  it('should get recipes that includes search term', () => {
+    const expected1 = repository.getRecipeIngredientsData('instant vanilla pudding');
+    const expected2 = repository.getRecipeIngredientsData('wheat flour');
+    const expected3 = repository.getRecipeIngredientsData('proscuitto');
+
+    expect(expected1).to.deep.equal([repository.recipes[0]])
+    expect(expected2).to.deep.equal([repository.recipes[0], repository.recipes[1]])
+    expect(expected3).to.deep.equal([repository.recipes[2]])
+  });
+
+  it('should return an empty array if no recipes match the search term', () => {
+    const expected = repository.getRecipeIngredientsData('broccoli');
+
+    expect(expected).to.deep.equal([]);
+    expect(expected.name).to.equal(undefined);
+  })
+
+  it('should get all data for a given recipe', () => {
+    const expected1 = repository.getRecipesInfo();
+    const expected2 = repository.getRecipesInfo();
+    const expected3 = repository.getRecipesInfo();
 
     expect(expected1).to.deep.equal(repository.recipes[0]);
     expect(expected2).to.deep.equal(repository.recipes[1]);
