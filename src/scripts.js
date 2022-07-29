466c6a30cb69e05d7144cbecb9a68bb2a4750558
Emily Miles <ms.emily.j.miles@gmail.com>
2022-07-29 17:33:06 -0400

    Add display functions for the recipe pages/update the html and css to match

--- src/scripts.js
+++ src/scripts.js
@@ -10,29 +10,49 @@ import User from './classes/User';
 
 
 // Query Selectors for buttons:
-let viewHomeButton = document.querySelector('.view-home-button');
-let viewAllRecipesButton = document.querySelector('.view-all-recipes');
-let viewFavoritesButton = document.querySelector('.view-favorites');
-let searchButton = document.querySelector('.search-submit-button');
-let searchInput = document.querySelector('.search-input');
-let snacksButton = document.querySelector('.snacks-button');
-let appetizersButton = document.querySelector('.appetizers-button');
-let sideDishesButton = document.querySelector('.side-dishes-button');
-let soupsButton = document.querySelector('.soups-button');
-let saladsButton = document.querySelector('.salads-button');
-let breakfastButton = document.querySelector('.breakfast-button');
-let lunchButton = document.querySelector('.lunch-button');
-let dinnerButton = document.querySelector('.dinner-button');
-let dessertButton = document.querySelector('.dessert-button');
+const viewHomeButton = document.querySelector('.view-home-button');
+const viewAllRecipesButton = document.querySelector('.view-all-recipes');
+const viewFavoritesButton = document.querySelector('.view-favorites');
+const searchButton = document.querySelector('.search-submit-button');
+const searchInput = document.querySelector('.search-input');
+const breakfastButton = document.querySelector('.breakfast-button');
+const dipsButton = document.querySelector('.dips-button');
+const appetizersButton = document.querySelector('.appetizers-button');
+const saucesButton = document.querySelector('.sauces-button');
+const saladsButton = document.querySelector('.salads-button');
+const mainCoursesButton = document.querySelector('.main-course-button');
+const sideDishesButton = document.querySelector('.side-dishes-button');
+const otherRecipesButton = document.querySelector('.other-recipes-button');
 
-// Query Selectors for full page views:
-let homeView = document.querySelector('.home-view-section');
-let allRecipesView = document.querySelector('.all-recipes-view-section');
-let allRecipesContent = document.querySelector('.all-recipes-view-content')
-let favoritesView = document.querySelector('.favorites-view-section');
-let searchResultsView = document.querySelector('.search-results-view-section');
-let singleRecipeView = document.querySelector('.single-recipe-view-section');
+// Query Selectors for views:
+const homeView = document.querySelector('.home-view-section');
+const allRecipesView = document.querySelector('.all-recipes-view-section');
+const favoritesView = document.querySelector('.favorites-view-section');
+const searchResultsView = document.querySelector('.search-results-view-section');
+const singleRecipeView = document.querySelector('.single-recipe-view-section');
+const breakfastRecipesView = document.querySelector('.breakfast-recipes-view-section');
+const dipRecipesView = document.querySelector('.dips-recipes-view-section');
+const appetizerRecipesView = document.querySelector('.appetizers-recipes-view-section');
+const sauceRecipesView = document.querySelector('.sauces-recipes-view-section');
+const saladRecipesView = document.querySelector('.salads-recipes-view-section');
+const mainCourseRecipesView = document.querySelector('.main-course-recipes-view-section');
+const sideDishRecipesView = document.querySelector('.side-dishes-recipes-view-section');
+const otherRecipesView = document.querySelector('.other-recipes-view-section');
 
+// Query Selector for updating page content
+const allRecipesContent = document.querySelector('.all-recipes-view-content')
+const favoritesContent = document.querySelector('.favorites-view-content')
+const searchResultsContent = document.querySelector('.search-results-content')
+const singleRecipeTitle = document.querySelector('.single-recipe-title')
+const singleRecipeContent = document.querySelector('.single-recipe-content')
+const breakfastRecipesContent = document.querySelector('.breakfast-recipes-content');
+const dipRecipesContent = document.querySelector('.dips-recipes-content');
+const appetizerRecipesContent = document.querySelector('.appetizers-recipes-content');
+const sauceRecipesContent = document.querySelector('.sauces-recipes-content');
+const saladRecipesContent = document.querySelector('.salads-recipes-content');
+const mainCourseRecipesContent = document.querySelector('.main-course-recipes-content');
+const sideDishRecipesContent = document.querySelector('.side-dishes-recipes-content');
+const otherRecipesContent = document.querySelector('.other-recipes-content');
 
 // Global Variables:
 let ingredients;
@@ -82,15 +102,14 @@ viewHomeButton.addEventListener('click', displayHomeView);
 viewAllRecipesButton.addEventListener('click', displayAllRecipesView);
 viewFavoritesButton.addEventListener('click', displayFavoritesView);
 searchButton.addEventListener('click', displaySearchResultsView)
-snacksButton.addEventListener('click', displaySnackRecipes)
-appetizersButton.addEventListener('click', displayAppetizerRecipes)
-sideDishesButton.addEventListener('click', displaySideDishRecipes)
-soupsButton.addEventListener('click', displaySoupRecipes)
-saladsButton.addEventListener('click', displaySaladRecipes)
 breakfastButton.addEventListener('click', displayBreakfastRecipes)
-lunchButton.addEventListener('click', displayLunchRecipes)
-dinnerButton.addEventListener('click', displayDinnerRecipes)
-dessertButton.addEventListener('click', displayDessertRecipes)
+dipsButton.addEventListener('click', displayDipRecipes)
+appetizersButton.addEventListener('click', displayAppetizerRecipes)
+saucesButton.addEventListener('click', displaySauceRecipes)
+saladsButton.addEventListener('click', displaySaladRecipes)
+// mainCoursesButton.addEventListener('click', displayMainCourseRecipes)
+sideDishesButton.addEventListener('click', displaySideDishRecipes)
+// otherRecipesButton.addEventListener('click', displayOtherRecipes)
 
 // Helper Functions:
 function hideElements(elements) {
@@ -109,80 +128,75 @@ function showElements(elements) {
 //   elements.classList.remove(className)
 // };
 
-// DOM Updates
+// DOM Display
 function displayHomeView() {
-  hideElements([viewHomeButton, allRecipesView, favoritesView, searchResultsView, singleRecipeView]);
+  hideElements([viewHomeButton, allRecipesView, favoritesView, searchResultsView, singleRecipeView, breakfastRecipesView, dipRecipesView, appetizerRecipesView, sauceRecipesView, saladRecipesView, mainCourseRecipesView, sideDishRecipesView, otherRecipesView]);
   showElements([homeView, viewAllRecipesButton, viewFavoritesButton]);
-}
+};
 
 function displayAllRecipesView() {
-  hideElements([homeView, viewAllRecipesButton, favoritesView, searchResultsView, singleRecipeView]);
+  hideElements([homeView, viewAllRecipesButton, favoritesView, searchResultsView, singleRecipeView, breakfastRecipesView, dipRecipesView, appetizerRecipesView, sauceRecipesView, saladRecipesView, mainCourseRecipesView, sideDishRecipesView, otherRecipesView]);
   showElements([allRecipesView, viewHomeButton, viewFavoritesButton]);
   createRecipeCard(allRecipesContent, recipeRepository);
 };
 
 function displayFavoritesView() {
-  hideElements([homeView, allRecipesView, viewFavoritesButton, searchResultsView, singleRecipeView]);
+  hideElements([homeView, allRecipesView, viewFavoritesButton, searchResultsView, singleRecipeView, breakfastRecipesView, dipRecipesView, appetizerRecipesView, sauceRecipesView, saladRecipesView, mainCourseRecipesView, sideDishRecipesView, otherRecipesView]);
   showElements([favoritesView, viewHomeButton, viewAllRecipesButton]);
-}
+};
 
 function displaySearchResultsView() {
-  hideElements([homeView, allRecipesView, favoritesView, singleRecipeView]);
+  hideElements([homeView, allRecipesView, favoritesView, singleRecipeView, breakfastRecipesView, dipRecipesView, appetizerRecipesView, sauceRecipesView, saladRecipesView, mainCourseRecipesView, sideDishRecipesView, otherRecipesView]);
   showElements([searchResultsView, viewHomeButton, viewAllRecipesButton, viewFavoritesButton]);
-}
+};
 
 function displaySingleRecipeView() {
-  hideElements([homeView, allRecipesView, favoritesView, searchResultsView]);
+  hideElements([homeView, allRecipesView, favoritesView, searchResultsView, breakfastRecipesView, dipRecipesView, appetizerRecipesView, sauceRecipesView, saladRecipesView, mainCourseRecipesView, sideDishRecipesView, otherRecipesView]);
   showElements([singleRecipeView, viewHomeButton, viewAllRecipesButton, viewFavoritesButton]);
 
-}
-
-function displaySnackRecipes() {
-  hideElements([homeView, allRecipesView, favoritesView, searchResultsView, singleRecipeView]);
-  showElements([viewHomeButton, viewAllRecipesButton, viewFavoritesButton]);
-}
-
-function displayAppetizerRecipes() {
-  hideElements([homeView, allRecipesView, favoritesView, searchResultsView, singleRecipeView]);
-  showElements([viewHomeButton, viewAllRecipesButton, viewFavoritesButton]);
-}
-
-function displaySideDishRecipes() {
-  hideElements([homeView, allRecipesView, favoritesView, searchResultsView, singleRecipeView]);
-  showElements([viewHomeButton, viewAllRecipesButton, viewFavoritesButton]);
-}
-
-function displaySoupRecipes() {
-  hideElements([homeView, allRecipesView, favoritesView, searchResultsView, singleRecipeView]);
-  showElements([viewHomeButton, viewAllRecipesButton, viewFavoritesButton]);
-}
-
-function displaySaladRecipes() {
-  hideElements([homeView, allRecipesView, favoritesView, searchResultsView, singleRecipeView]);
-  showElements([viewHomeButton, viewAllRecipesButton, viewFavoritesButton]);
-}
+};
 
 function displayBreakfastRecipes() {
-  hideElements([homeView, allRecipesView, favoritesView, searchResultsView, singleRecipeView]);
-  showElements([viewHomeButton, viewAllRecipesButton, viewFavoritesButton]);
-}
+  hideElements([homeView, allRecipesView, favoritesView, searchResultsView, singleRecipeView, dipRecipesView, appetizerRecipesView, sauceRecipesView, saladRecipesView, mainCourseRecipesView, sideDishRecipesView, otherRecipesView]);
+  showElements([breakfastRecipesView, viewHomeButton, viewAllRecipesButton, viewFavoritesButton]);
+};
 
-function displayLunchRecipes() {
-  hideElements([homeView, allRecipesView, favoritesView, searchResultsView, singleRecipeView]);
-  showElements([viewHomeButton, viewAllRecipesButton, viewFavoritesButton]);
-}
+function displayDipRecipes() {
+  hideElements([homeView, allRecipesView, favoritesView, searchResultsView, singleRecipeView, breakfastRecipesView, appetizerRecipesView, sauceRecipesView, saladRecipesView, mainCourseRecipesView, sideDishRecipesView, otherRecipesView]);
+  showElements([dipRecipesView, viewHomeButton, viewAllRecipesButton, viewFavoritesButton]);
+};
 
-function displayDinnerRecipes() {
-  hideElements([homeView, allRecipesView, favoritesView, searchResultsView, singleRecipeView]);
-  showElements([viewHomeButton, viewAllRecipesButton, viewFavoritesButton]);
-}
+function displayAppetizerRecipes() {
+  hideElements([homeView, allRecipesView, favoritesView, searchResultsView, singleRecipeView, breakfastRecipesView, dipRecipesView, sauceRecipesView, saladRecipesView, mainCourseRecipesView, sideDishRecipesView, otherRecipesView]);
+  showElements([appetizerRecipesView, viewHomeButton, viewAllRecipesButton, viewFavoritesButton]);
+};
 
-function displayDessertRecipes() {
-  hideElements([homeView, allRecipesView, favoritesView, searchResultsView, singleRecipeView]);
-  showElements([viewHomeButton, viewAllRecipesButton, viewFavoritesButton]);
-}
+function displaySauceRecipes() {
+  hideElements([homeView, allRecipesView, favoritesView, searchResultsView, singleRecipeView, breakfastRecipesView, dipRecipesView, appetizerRecipesView, saladRecipesView, mainCourseRecipesView, sideDishRecipesView, otherRecipesView]);
+  showElements([sauceRecipesView, viewHomeButton, viewAllRecipesButton, viewFavoritesButton]);
+};
 
-// Functionality
+function displaySaladRecipes() {
+  hideElements([homeView, allRecipesView, favoritesView, searchResultsView, singleRecipeView, breakfastRecipesView, dipRecipesView, appetizerRecipesView, sauceRecipesView, mainCourseRecipesView, sideDishRecipesView, otherRecipesView]);
+  showElements([saladRecipesView, viewHomeButton, viewAllRecipesButton, viewFavoritesButton]);
+};
+
+// function displayMainCourseRecipes() {
+//   hideElements([homeView, allRecipesView, favoritesView, searchResultsView, singleRecipeView, breakfastRecipesView, dipRecipesView, appetizerRecipesView, sauceRecipesView, saladRecipesView, sideDishRecipesView, otherRecipesView]);
+//   showElements([mainCourseRecipesView, viewHomeButton, viewAllRecipesButton, viewFavoritesButton]);
+// };
+
+function displaySideDishRecipes() {
+  hideElements([homeView, allRecipesView, favoritesView, searchResultsView, singleRecipeView, breakfastRecipesView, dipRecipesView, appetizerRecipesView, sauceRecipesView, saladRecipesView, mainCourseRecipesView, otherRecipesView]);
+  showElements([sideDishRecipesView, viewHomeButton, viewAllRecipesButton, viewFavoritesButton]);
+};
+
+// function displayOtherRecipes() {
+//   hideElements([homeView, allRecipesView, favoritesView, searchResultsView, singleRecipeView, breakfastRecipesView, dipRecipesView, appetizerRecipesView, sauceRecipesView, saladRecipesView, mainCourseRecipesView, sideDishRecipesView]);
+//   showElements([otherRecipesView, viewHomeButton, viewAllRecipesButton, viewFavoritesButton]);
+// };
+
+// DOM Functionality
 function getUser(usersData) {
   const randomIndex = getRandomIndex(usersData);
   const randomUserData = usersData[randomIndex];
